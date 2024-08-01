ccaeb2282996a715f038bc1991ab048d16c2f9a3
Bohdan-pidkovych <52783471+Bohdan-pidkovych@users.noreply.github.com>
2024-08-01 18:38:16 +0300

    Created mail form, footer, scrollbar

--- js/main.js
+++ js/main.js
@@ -32,6 +32,16 @@ const contactMessage = document.querySelector('.contact__message');
 
 const sendEmail = (evt) => {
   evt.preventDefault();
+  emailjs.sendForm('service_f6kmqef', 'template_itdn71g', '#contact-form', 'gscMX-75JGSJT5afZ')
+    .then(() => {
+      contactMessage.textContent = 'Message sent successfully';
+      setTimeout(() => {
+        contactMessage.textContent = '';
+      }, 5000);
+      contactForm.reset();
+    }, () => {
+      contactMessage.textContent = 'Message not sent (service error)';
+    })
 }
 
 contactForm.addEventListener('submit', sendEmail);
